ec5e5fab31ccd5e0595b710e0717b6e36e6aa939
Andrew Plumb <andrew@plumb.org>
2010-11-12 14:47:07 -0500

    Testing basic shell-creation process on a simple circle/ellipse.  It works!

--- src/tests/QPolygonTests/main.cpp
+++ src/tests/QPolygonTests/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <QApplication>
 #include <QDebug>
 #include <QFileDialog>
@@ -7,6 +8,7 @@
 #include <QPen>
 #include <QPolygon>
 #include <QPolygonF>
+#include <QRect>
 #include <QRegion>
 #include <QString>
 #include <QSvgGenerator>
@@ -56,6 +58,13 @@ int main(int argc, char *argv[]) {
   path3=path2.subtracted(path1);
   QPainterPath path4=QPainterPath(path3);
   QPainterPath path5=QPainterPath(path3);
+  QPainterPath path6=QPainterPath(path3);
+
+  QPainterPath path7;
+  path7.addRect(-10,-10,20,20);
+  QPainterPath path8;
+  path8.addRect(-5,-5,10,10);
+  QPainterPath path9=path7.subtracted(path8);
 
   poly3.translate(20,20);
   poly4.translate(40,20);
@@ -65,12 +74,19 @@ int main(int argc, char *argv[]) {
   path3.translate(60,40);
   path4.translate(20,60);
   path5.translate(40,60);
+  path6.translate(60,60);
+
+  //path7.translate(20,80);
+  // path8.translate(40,80);
+
   if (useGUI) {
     // start GUI version
     qDebug() << " GUI Mode\n";
     QPainter painter;
     painter.begin(&generator);
     painter.setPen(Qt::blue);
+    painter.fillRect(QRect(0,0,200,200), Qt::gray);
+    painter.setBrush(Qt::green);
     painter.drawPolygon(poly1);
     painter.drawPolygon(poly2);
 
@@ -84,11 +100,62 @@ int main(int argc, char *argv[]) {
     foreach (QPolygonF thisPoly, path4.toFillPolygons()) {
       painter.drawPolygon(thisPoly);
     }
-
     path5.setFillRule(Qt::WindingFill);
     foreach (QPolygonF thisPoly, path5.toSubpathPolygons()) {
       painter.drawPolygon(thisPoly);
     }
+    path6.setFillRule(Qt::OddEvenFill);
+    foreach (QPolygonF thisPoly, path6.toSubpathPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+
+    path9.translate(20,80);
+    foreach (QPolygonF thisPoly, path9.toFillPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+
+    path9.translate(20,0);
+    path9.setFillRule(Qt::WindingFill);
+    foreach (QPolygonF thisPoly, path9.toFillPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+    path9.translate(20,0);
+    path9.setFillRule(Qt::OddEvenFill);
+    foreach (QPolygonF thisPoly, path9.toFillPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+
+    painter.setBrush(Qt::NoBrush);
+    path9.translate(-40,20);
+    path9.setFillRule(Qt::WindingFill);
+    foreach (QPolygonF thisPoly, path9.toSubpathPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+    path9.translate(20,0);
+    path9.setFillRule(Qt::OddEvenFill);
+    foreach (QPolygonF thisPoly, path9.toSubpathPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+
+    QPainterPath pathCircle;
+    pathCircle.addEllipse(0,0,20,20);
+    QPainterPath shellPath;
+    qreal shellWidth=2.0;
+    int shellCount=8;
+    for(int i=0;i<shellCount;i++) {
+      qreal rotAngle=360*i/shellCount;
+      QPointF shiftPoint(shellWidth*cos(rotAngle),shellWidth*sin(rotAngle));
+      QPainterPath shiftPath=pathCircle.translated(shiftPoint);
+      QPainterPath partShell=pathCircle.subtracted(shiftPath);
+      shellPath=shellPath.united(partShell);
+    }
+    shellPath.translate(20,120);
+    shellPath.setFillRule(Qt::OddEvenFill);
+    painter.setBrush(Qt::red);
+    foreach (QPolygonF thisPoly, shellPath.toFillPolygons()) {
+      painter.drawPolygon(thisPoly);
+    }
+      
     painter.end();
   } else {
     // start non-GUI version
